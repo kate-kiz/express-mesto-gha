8ad0c2974d26a2e9ecc559fe8677f44640cabfa9
Ekaterina Kizikova <kizikovaek@yandex.ru>
2023-07-09 17:41:08 +0700

    3rd commit

--- controllers/users.js
+++ controllers/users.js
@@ -6,8 +6,10 @@ module.exports.getUserById = (userId) => User.findById(userId);
 
 module.exports.createUser = ({ name, about, avatar }) => User.create({ name, about, avatar });
 
-module.exports.updateAvatar = (userId, avatar) =>
-  User.findByIdAndUpdate(userId, { avatar }, { new: true, runValidators: true });
+module.exports.updateAvatar = (userId, avatar) => (
+  User.findByIdAndUpdate(userId, { avatar }, { new: true, runValidators: true })
+);
 
-module.exports.updateProfile = (userId, name, about) =>
-  User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true });
+module.exports.updateProfile = (userId, name, about) => (
+  User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true })
+);
